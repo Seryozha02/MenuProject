a156a87be6390926b2f7da7c76ea24b19f671fe4
Seryozha02 <seryozha.asatryan02@gmail.com>
2022-02-20 01:08:43 +0400

    MenuProject

---
+++ .history/menuFilter/app_20220217232927.js
@@ -0,0 +1,64 @@
+import { menu } from "./data.js";
+
+// get parent element
+const sectionCenter = document.querySelector(".section-center");
+const btnContainer = document.querySelector(".btn-container");
+// display all items when page loads
+window.addEventListener("DOMContentLoaded", function () {
+  displayMenuItems(menu);
+  displayMenuButtons();
+});
+
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function (item) {
+    // console.log(item);
+
+    return `<article class="menu-item">
+            <img src=${item.img} alt=${item.title} class="photo" />
+            <div class="item-info">
+              <header>
+                <h4>${item.title}</h4>
+                <h4 class="price">$${item.price}</h4>
+              </header>
+              <p class="item-text">
+                ${item.desc}
+              </p>
+            </div>
+          </article>`;
+  });
+  displayMenu = displayMenu.join("");
+  // console.log(displayMenu);
+
+  sectionCenter.innerHTML = displayMenu;
+}
+
+function displayMenuButtons() {
+  //add display buttons here
+}
+const buttons = document.querySelectorAll(".filter-btn");
+buttons.forEach(filterBtn  => {
+  filterBtn.addEventListener("click", () => {
+    const value = filterBtn.getAttribute("data-id");
+    if(value === "all"){
+      displayMenuItems(menu);
+      return;
+    }
+    const filterArr = menu.filter(item => item.category === value);
+    let cart = filterArr.map(function(item){
+      return `<article class="menu-item">
+            <img src=${item.img} alt=${item.title} class="photo" />
+            <div class="item-info">
+              <header>
+                <h4>${item.title}</h4>
+                <h4 class="price">$${item.price}</h4>
+              </header>
+              <p class="item-text">
+                ${item.desc}
+              </p>
+            </div>
+          </article>`;
+
+    })
+  })
+})
+
